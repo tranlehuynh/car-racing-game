9584bc87c7ff578727e90c9e44575e6fc8039094
tranl <tranl@DESKTOP-UAKPBVK>
2021-08-21 18:39:35 +0700

    Add project files.

---
+++ car-racing-game/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace car_racing_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.ptbMainCar = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.ptbEnemyCar1 = new System.Windows.Forms.PictureBox();
+            this.ptbEnemyCar3 = new System.Windows.Forms.PictureBox();
+            this.ptbEnemyCar4 = new System.Windows.Forms.PictureBox();
+            this.ptbEnemyCar2 = new System.Windows.Forms.PictureBox();
+            this.ptbEnemyCar5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbMainCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbEnemyCar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbEnemyCar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbEnemyCar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbEnemyCar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbEnemyCar5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(234, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 109);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(234, 142);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(10, 109);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Location = new System.Drawing.Point(234, 426);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(10, 109);
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox4.Location = new System.Drawing.Point(234, 286);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(10, 109);
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            // 
+            // ptbMainCar
+            // 
+            this.ptbMainCar.BackColor = System.Drawing.Color.Gray;
+            this.ptbMainCar.Image = ((System.Drawing.Image)(resources.GetObject("ptbMainCar.Image")));
+            this.ptbMainCar.Location = new System.Drawing.Point(361, 416);
+            this.ptbMainCar.Name = "ptbMainCar";
+            this.ptbMainCar.Size = new System.Drawing.Size(52, 87);
+            this.ptbMainCar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbMainCar.TabIndex = 1;
+            this.ptbMainCar.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox5.Location = new System.Drawing.Point(593, 12);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(10, 109);
+            this.pictureBox5.TabIndex = 0;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.White;
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox6.Location = new System.Drawing.Point(593, 142);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(10, 109);
+            this.pictureBox6.TabIndex = 0;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.White;
+            this.pictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox7.Location = new System.Drawing.Point(593, 286);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(10, 109);
+            this.pictureBox7.TabIndex = 0;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.White;
+            this.pictureBox8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox8.Location = new System.Drawing.Point(593, 426);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(10, 109);
+            this.pictureBox8.TabIndex = 0;
+            this.pictureBox8.TabStop = false;
+            // 
+            // ptbEnemyCar1
+            // 
+            this.ptbEnemyCar1.BackColor = System.Drawing.Color.Gray;
+            this.ptbEnemyCar1.Image = ((System.Drawing.Image)(resources.GetObject("ptbEnemyCar1.Image")));
+            this.ptbEnemyCar1.Location = new System.Drawing.Point(92, 177);
+            this.ptbEnemyCar1.Name = "ptbEnemyCar1";
+            this.ptbEnemyCar1.Size = new System.Drawing.Size(52, 87);
+            this.ptbEnemyCar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbEnemyCar1.TabIndex = 1;
+            this.ptbEnemyCar1.TabStop = false;
+            // 
+            // ptbEnemyCar3
+            // 
+            this.ptbEnemyCar3.BackColor = System.Drawing.Color.Gray;
+            this.ptbEnemyCar3.Image = ((System.Drawing.Image)(resources.GetObject("ptbEnemyCar3.Image")));
+            this.ptbEnemyCar3.Location = new System.Drawing.Point(512, 50);
+            this.ptbEnemyCar3.Name = "ptbEnemyCar3";
+            this.ptbEnemyCar3.Size = new System.Drawing.Size(52, 87);
+            this.ptbEnemyCar3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbEnemyCar3.TabIndex = 1;
+            this.ptbEnemyCar3.TabStop = false;
+            // 
+            // ptbEnemyCar4
+            // 
+            this.ptbEnemyCar4.BackColor = System.Drawing.Color.Gray;
+            this.ptbEnemyCar4.Image = ((System.Drawing.Image)(resources.GetObject("ptbEnemyCar4.Image")));
+            this.ptbEnemyCar4.Location = new System.Drawing.Point(647, 177);
+            this.ptbEnemyCar4.Name = "ptbEnemyCar4";
+            this.ptbEnemyCar4.Size = new System.Drawing.Size(52, 87);
+            this.ptbEnemyCar4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbEnemyCar4.TabIndex = 1;
+            this.ptbEnemyCar4.TabStop = false;
+            // 
+            // ptbEnemyCar2
+            // 
+            this.ptbEnemyCar2.BackColor = System.Drawing.Color.Gray;
+            this.ptbEnemyCar2.Image = ((System.Drawing.Image)(resources.GetObject("ptbEnemyCar2.Image")));
+            this.ptbEnemyCar2.Location = new System.Drawing.Point(289, 114);
+            this.ptbEnemyCar2.Name = "ptbEnemyCar2";
+            this.ptbEnemyCar2.Size = new System.Drawing.Size(52, 87);
+            this.ptbEnemyCar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbEnemyCar2.TabIndex = 1;
+            this.ptbEnemyCar2.TabStop = false;
+            // 
+            // ptbEnemyCar5
+            // 
+            this.ptbEnemyCar5.BackColor = System.Drawing.Color.Gray;
+            this.ptbEnemyCar5.Image = ((System.Drawing.Image)(resources.GetObject("ptbEnemyCar5.Image")));
+            this.ptbEnemyCar5.Location = new System.Drawing.Point(666, 355);
+            this.ptbEnemyCar5.Name = "ptbEnemyCar5";
+            this.ptbEnemyCar5.Size = new System.Drawing.Size(52, 87);
+            this.ptbEnemyCar5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbEnemyCar5.TabIndex = 1;
+            this.ptbEnemyCar5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(803, 523);
+            this.Controls.Add(this.ptbEnemyCar4);
+            this.Controls.Add(this.ptbEnemyCar5);
+            this.Controls.Add(this.ptbEnemyCar2);
+            this.Controls.Add(this.ptbEnemyCar3);
+            this.Controls.Add(this.ptbEnemyCar1);
+            this.Controls.Add(this.ptbMainCar);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbMainCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbEnemyCar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbEnemyCar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbEnemyCar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbEnemyCar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbEnemyCar5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox ptbMainCar;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox ptbEnemyCar1;
+        private System.Windows.Forms.PictureBox ptbEnemyCar3;
+        private System.Windows.Forms.PictureBox ptbEnemyCar4;
+        private System.Windows.Forms.PictureBox ptbEnemyCar2;
+        private System.Windows.Forms.PictureBox ptbEnemyCar5;
+    }
+}
+
---
+++ car-racing-game/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace car_racing_game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string path;
+        Random rand = new Random();
+        int x, y;
+        void getTime(int speed)
+        {
+            if (pictureBox1.Top >= 440) {pictureBox1.Top = -2;}
+            else { pictureBox1.Top += speed; }
+
+            if (pictureBox2.Top >= 440) { pictureBox2.Top = -2; }
+            else { pictureBox2.Top += speed; }
+
+            if (pictureBox3.Top >= 440) { pictureBox3.Top = -2; }
+            else { pictureBox3.Top += speed; }
+
+            if (pictureBox4.Top >= 440) { pictureBox4.Top = -2; }
+            else { pictureBox4.Top += speed; }
+
+            if (pictureBox5.Top >= 440) { pictureBox5.Top = -2; }
+            else { pictureBox5.Top += speed; }
+
+            if (pictureBox6.Top >= 440) { pictureBox6.Top = -2; }
+            else { pictureBox6.Top += speed; }
+
+            if (pictureBox7.Top >= 440) { pictureBox7.Top = -2; }
+            else { pictureBox7.Top += speed; }
+
+            if (pictureBox8.Top >= 440) { pictureBox8.Top = -2; }
+            else { pictureBox8.Top += speed; }
+        }
+        void enemyCar(int speed)
+        {
+            if (ptbEnemyCar1.Top > 400)
+            {
+                x = rand.Next(0, 300);
+                ptbEnemyCar1.Location = new Point(x, 0);
+            }
+            else { ptbEnemyCar1.Top += speed; }
+
+            if (ptbEnemyCar2.Top > 400)
+            {
+                x = rand.Next(0, 300);
+                ptbEnemyCar2.Location = new Point(x, 0);
+            }
+            else { ptbEnemyCar2.Top += speed; }
+
+            if (ptbEnemyCar3.Top > 400)
+            {
+                x = rand.Next(180, 350);
+                ptbEnemyCar3.Location = new Point(x, 0);
+            }
+            else { ptbEnemyCar3.Top += speed; }
+
+            if (ptbEnemyCar4.Top > 400)
+            {
+                x = rand.Next(300, 350);
+                ptbEnemyCar4.Location = new Point(x, 0);
+            }
+            else { ptbEnemyCar4.Top += speed; }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            getTime(8);
+            enemyCar(8);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                if (ptbMainCar.Left > 0)
+                    ptbMainCar.Left -= 10;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                if (ptbMainCar.Right < 720)
+                    ptbMainCar.Left += 10;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                if (ptbMainCar.Top > 0)
+                    ptbMainCar.Top -= 10;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                if (ptbMainCar.Top < 340)
+                    ptbMainCar.Top += 10;
+            }
+
+        }
+    }
+}
