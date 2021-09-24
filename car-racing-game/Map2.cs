82026352d5a98e7d8be15daf098c48feb38aa432
HuyNguyenDinh <61400397+HuyNguyenDinh@users.noreply.github.com>
2021-09-24 14:48:54 +0700

    Add comment

--- car-racing-game/Map2.cs
+++ car-racing-game/Map2.cs
@@ -13,34 +13,35 @@ namespace car_racing_game
     public partial class Map2 : Form
     {
         Int64 road = 0;
-        BG backGround;
+        BG backGround; //Gồm lane và vạch kẻ đường đã thiết kế sẵn
         MainCar mainCar;
-        EnemyCar[] enemies;
+        EnemyCar[] enemies; //enemies này là tập hợp của các enemyCar
         public Map2()
         {
             InitializeComponent();
+            // Hàm BG với tham số (speed của map, mảng các vạch đứt trong map đã được thiết kế sẵn, mảng các vạch liền, mảng các toạ độ X của từng lane dựa vào toạ độ này để xác định lane cho Car)
             backGround = new BG(5, new PictureBox[] { pbVachDut1, pbVachDut2, pbVachDut3, pbVachDut4, pbVachDut5, pbVachDut6, pbVachDut7, pbVachDut8, pbVachDut9, pbVachDut10, pbVachDut11, pbVachDut12}
-            ,new PictureBox[] { pictureBox1}
-            , new int[] { 112, 302, 492, 672 });
-            mainCar = new MainCar(pbMainCar, 0, ClientRectangle, backGround);
-            EnemyCar enemyCar1 = new EnemyCar(pbEnemyCar1, backGround.speed, backGround);
+            ,new PictureBox[] { pictureBox1} // Vạch liền
+            , new int[] { 112, 302, 492, 672 }); // left của từng lane
+            mainCar = new MainCar(pbMainCar, 0, ClientRectangle, backGround); // Hàm khởi tạo cần pictureBox của mainCar
+            EnemyCar enemyCar1 = new EnemyCar(pbEnemyCar1, backGround.speed, backGround); // EnemyCar cần pictureBox của enemyCar, speed của map và đối tượng backGround (hiện tại trong code đang là backGround thiết kế với 4 lane)
             EnemyCar enemyCar2 = new EnemyCar(pbEnemyCar2, backGround.speed, backGround);
             EnemyCar enemyCar3 = new EnemyCar(pbEnemyCar3, backGround.speed, backGround);
             EnemyCar enemyCar4 = new EnemyCar(pbEnemyCar4, backGround.speed, backGround);
-            enemies = new EnemyCar[] { enemyCar1, enemyCar2, enemyCar3, enemyCar4 };
+            enemies = new EnemyCar[] { enemyCar1, enemyCar2, enemyCar3, enemyCar4 }; //Thêm các enemyCar vào tập hợp enemies để dễ kiểm soát
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            road += backGround.speed;
-            Point.Text = (road / 1000).ToString(); //1000 có thể thay đổi
-            backGround.loopmove(ClientRectangle);
+            road += backGround.speed; //Tính quãng đường đi được, hiện bên tay trái form
+            Point.Text = (road / 1000).ToString(); //1000 có thể thay đổi //Tính điểm theo cơ chế road = 1000 thì được 1 điểm
+            backGround.loopmove(ClientRectangle); // Hàm loopmove dùng để di chuyển các vạch đứt trong vạch kẻ đường, vạch kẻ đường đã được thêm vào bằng hàm khởi tạo
             foreach(var item in enemies)
             {
-                item.move(ClientRectangle, backGround, enemies);
+                item.move(ClientRectangle, backGround, enemies); //EnemyCar.move dùng để di chuyển các enemyCar, đã xử lý việc random xuất hiện
             }
-            EnemyCar.EnemyCarVsEnemyCar(enemies);
-            if (mainCar.vaChamXe(enemies))
+            EnemyCar.EnemyCarVsEnemyCar(enemies); // Kiểm tra xem các enemyCar có đụng trúng nhau không, nếu có thì hãm speed của enemyCar lại
+            if (mainCar.vaChamXe(enemies)) // Kiểm tra đụng xe giữa mainCar và các enemyCar, tham số enemies là tập hợp các enemyCar
             {
                 timer1.Enabled = false;
             }
