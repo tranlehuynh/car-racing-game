8db2655c1ddd239011869fd5f6293b656720cc3e
HuyNguyenDinh <61400397+HuyNguyenDinh@users.noreply.github.com>
2021-09-24 13:18:13 +0700

    Update map2 and class car

---
+++ Car.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_racing_game
+{
+    class Car
+    {
+        public System.Windows.Forms.PictureBox pb;
+        public int speed;
+        public Car(System.Windows.Forms.PictureBox pictureBox, int speed) 
+        {
+            this.pb = pictureBox;
+            this.speed = speed;
+        }
+    }
+    class MainCar : Car
+    {
+        public MainCar(System.Windows.Forms.PictureBox pictureBox, int speed, System.Drawing.Rectangle rectangle, BG bG) : base(pictureBox, speed)
+        {
+            pb.Top = rectangle.Bottom - pb.Height;
+            pb.Left = bG.lane[bG.lane.Length/2];
+        }
+
+
+        public void move(BG bG, bool left)
+        {
+            int vt = Array.IndexOf(bG.lane, pb.Left);
+            if (left)
+            {
+                if (pb.Left != bG.lane[0])
+                pb.Left = bG.lane[vt - 1];
+            }
+            else
+            {
+                if (pb.Left != bG.lane[bG.lane.Length-1])
+                pb.Left = bG.lane[vt + 1];
+            }
+        }
+        public bool vaChamXe(EnemyCar[] enemies)
+        {
+            foreach (var enemy in enemies)
+            {
+                if (this.pb.Bounds.IntersectsWith(enemy.pb.Bounds)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    class EnemyCar : Car
+    {
+        public bool nguoc = false;
+        public bool available = true;
+        
+        public EnemyCar(System.Windows.Forms.PictureBox pictureBox, int speed, BG bG) : base(pictureBox, speed)
+        {
+            pb.Top = -pb.Height;
+        }
+        public void makeEnemy(BG bG, EnemyCar[] enemies) // Tao enemyCar neu xe chua xuat hien hoac da di qua
+        {
+            Random random = new Random();
+            int temp = random.Next(0, (bG.lane.Length));
+            pb.Left = bG.lane[temp]; // lay lane random trong Background.lane
+            pb.Top = 0 - pb.Height;
+            foreach (var enemy in enemies)
+            {
+                if (this.Equals(enemy)) // kiểm tra trùng
+                    continue;
+                if (enemy.pb.Bounds.IntersectsWith(pb.Bounds))
+                {
+                    pb.Top = enemy.pb.Top - 2 * pb.Height;
+                }
+            }
+            if (temp < bG.lane.Length/2)
+            {
+                this.speed = random.Next(bG.speed, (int)(3.0 / 2.0 * bG.speed));
+                pb.Image.RotateFlip(System.Drawing.RotateFlipType.RotateNoneFlipY);
+                nguoc = true;
+            }
+            else
+            {
+                nguoc = false;
+                this.speed = random.Next((bG.speed / 2),bG.speed);
+            }
+        }
+        public static void EnemyCarVsEnemyCar(EnemyCar[] enemyCars)
+        {
+            foreach (var item in enemyCars)
+            {
+                foreach (var y in enemyCars)
+                {
+                    if (item.Equals(y))
+                        continue;
+                    if (item.pb.Bounds.IntersectsWith(y.pb.Bounds))
+                    {
+                        item.speed = y.speed;
+                    }
+                }
+            }
+        }
+
+        public void move(System.Drawing.Rectangle rec, BG bG, EnemyCar[] enemies)
+        {
+            if (!available)
+            {
+                pb.Top += speed;
+                if (pb.Top >= rec.Bottom)
+                {
+                    pb.Top = -pb.Height;
+                    available = true;
+                    if (nguoc)
+                    {
+                        pb.Image.RotateFlip(System.Drawing.RotateFlipType.RotateNoneFlipY);
+                        nguoc = false;
+                    }
+                }
+            }
+            else
+            {
+                available = false;
+                makeEnemy(bG, enemies);
+            }
+        }
+    }
+}
---
+++ Map2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace car_racing_game
+{
+    public partial class Map2 : Form
+    {
+        Int64 road = 0;
+        BG backGround;
+        MainCar mainCar;
+        EnemyCar[] enemies;
+        public Map2()
+        {
+            InitializeComponent();
+            backGround = new BG(5, new PictureBox[] { pbVachDut1, pbVachDut2, pbVachDut3, pbVachDut4, pbVachDut5, pbVachDut6, pbVachDut7, pbVachDut8, pbVachDut9, pbVachDut10, pbVachDut11, pbVachDut12}
+            ,new PictureBox[] { pictureBox1}
+            , new int[] { 112, 302, 492, 672 });
+            mainCar = new MainCar(pbMainCar, 0, ClientRectangle, backGround);
+            EnemyCar enemyCar1 = new EnemyCar(pbEnemyCar1, backGround.speed, backGround);
+            EnemyCar enemyCar2 = new EnemyCar(pbEnemyCar2, backGround.speed, backGround);
+            EnemyCar enemyCar3 = new EnemyCar(pbEnemyCar3, backGround.speed, backGround);
+            EnemyCar enemyCar4 = new EnemyCar(pbEnemyCar4, backGround.speed, backGround);
+            enemies = new EnemyCar[] { enemyCar1, enemyCar2, enemyCar3, enemyCar4 };
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            road += backGround.speed;
+            Point.Text = (road / 1000).ToString(); //1000 có thể thay đổi
+            backGround.loopmove(ClientRectangle);
+            foreach(var item in enemies)
+            {
+                item.move(ClientRectangle, backGround, enemies);
+            }
+            EnemyCar.EnemyCarVsEnemyCar(enemies);
+            if (mainCar.vaChamXe(enemies))
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        protected void timer2_Tick(object sender, EventArgs e)
+        {
+            Time.Text = (int.Parse(Time.Text) + 1).ToString();
+        }
+
+        protected void Map2_Load(object sender, EventArgs e)
+        {
+            Time.Text = Point.Text = 0.ToString();
+        }
+
+        private void Map2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                mainCar.move(backGround, true);
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                mainCar.move(backGround, false);
+            }
+        }
+    }
+}
