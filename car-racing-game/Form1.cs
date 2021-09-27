f5082e50ca28a78af7202c1205848580bcc36c36
Huynh Tran <tranlehuynhh@gmail.com>
2021-09-27 10:43:34 +0700

    Form1 update

--- car-racing-game/Form1.cs
+++ car-racing-game/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System;
 namespace car_racing_game
 {
     public partial class Form1 : Form
